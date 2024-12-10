dfd43c288689684f84f9428927d31213996ede9a
Hikarikedo <keriandouidi01@gmail.com>
2024-12-10 16:11:37 +0100

    fixed task 0, header, and task 1

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -1,6 +1,13 @@
 #include <stdlib.h>
 #include "binary_trees.h"
 
+/**
+ * binary_tree_node - creates an unprecised binary tree node
+ * @parent: pointer to the parent node of another node
+ * @value: the integer value to store in the new node
+ * Return: the pointer to the new node
+ */
+
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_node;

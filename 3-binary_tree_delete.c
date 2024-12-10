fc86b6d1f7031ab26695a24257f6f9f0d499eb27
root <ray.dotropg@gmail.com>
2024-12-10 16:08:49 +0000

    adding bety to 3 and adding task 4 betty including

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -1,6 +1,13 @@
 #include <stdlib.h>
 #include "binary_trees.h"
 
+/**
+ * binary_tree_delete - Deletes an entire binary tree
+ * @tree: Pointer to the root node of the tree to delete
+ *
+ * Description: Recursively deletes the left and right subtrees before
+ * deleting the current node.
+ */
 void binary_tree_delete(binary_tree_t *tree)
 {
 	if (tree == NULL)

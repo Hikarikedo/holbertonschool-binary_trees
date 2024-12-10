1096425a244b9f8825eb6ce9f0132cc0b8489b8d
Hikarikedo <keriandouidi01@gmail.com>
2024-12-10 16:19:32 +0100

    added right insert node and fixed betty on task 1

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -2,9 +2,9 @@
 
 /**
  * binary_tree_insert_left - inserts a node as the left-child of another node
- * @parent: pointer to the node to insert the left-child in
+ * @parent: pointer to the node to insert the left-child
  * @value: value to store in the new node
- * Return: pointer to the created node, or NULL on failure or if parent is NULL
+ * Return: pointer to the created node;NULL if the parent is NULL or on Failure
  */
 
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)

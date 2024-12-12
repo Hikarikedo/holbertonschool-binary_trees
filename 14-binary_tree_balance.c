1f56853a47bce648f5ce8ea89245f498cc643de1
root <ray.dotropg@gmail.com>
2024-12-12 01:48:46 +0000

    correction of 14 n5(i hope it's the last)

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -10,26 +10,23 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	size_t left_height, right_height;
 
 	if (tree == NULL)
-		return (0);
+		return (-1);
 
-	left_height = tree->left ? 1 + binary_tree_height(tree->left) : 0;
-	right_height = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
 
-	return (left_height > right_height ? left_height : right_height);
+	return ((left_height > right_height ? left_height : right_height) + 1);
 }
 
 int binary_tree_balance(const binary_tree_t *tree)
 {
-	size_t left_height, right_height;
+	int left_height, right_height;
 
 	if (tree == NULL)
 		return (0);
 
-	if (tree->left)
-		left_height = binary_tree_height(tree->left);
-
-	if (tree->right)
-		right_height = binary_tree_height(tree->right);
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
 
 	return (left_height - right_height);
 }

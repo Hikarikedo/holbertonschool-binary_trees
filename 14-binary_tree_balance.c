2826b7ea1f5eeb1c56377783c4fb1c8426c39be0
root <ray.dotropg@gmail.com>
2024-12-12 00:41:14 +0000

    correction of 14 n2

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -5,6 +5,22 @@
  * @tree: A pointer to the root node of the tree to count the nodes
  * Return: The number of nodes with at least 1 child, elsee return 0
  */
+size_t binary_tree_nodes(const binary_tree_t *tree)
+{
+	size_t count = 0;
+
+	if (tree == NULL)
+		return (0);
+
+	if (tree->left || tree->right)
+		count = 1 + binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right);
+
+	else
+		count = binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right);
+
+	return (count);
+}
+
 int binary_tree_balance(const binary_tree_t *tree)
 {
 	int left_height = 0, right_height = 0;

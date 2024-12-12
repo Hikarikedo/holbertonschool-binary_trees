e8bcde68db3d31dd76e7a7daedfd3b5ba3873672
root <ray.dotropg@gmail.com>
2024-12-12 00:55:53 +0000

    correction of 14 n4

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -20,7 +20,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 
 int binary_tree_balance(const binary_tree_t *tree)
 {
-	int left_height = 0, right_height = 0;
+	size_t left_height, right_height;
 
 	if (tree == NULL)
 		return (0);

299a95519604b2369214c0927f3b19612f40fabf
Hikarikedo <keriandouidi01@gmail.com>
2024-12-12 14:33:54 +0100

    finished 9 and 14

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,7 +1,7 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_nodes - Counts the nodes with at least 1 child in a binary tree
+ * binary_tree_height - Counts the nodes with at least 1 child in a binary tree
  * @tree: A pointer to the root node of the tree to count the nodes
  * Return: The number of nodes with at least 1 child, elsee return 0
  */
@@ -18,6 +18,11 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	return ((left_height > right_height ? left_height : right_height) + 1);
 }
 
+/**
+ * binary_tree_balance - measures the height of a binary tree
+ * @tree: pointer to the root of the tree
+ * Return: heught of the tree or 0 if the tree is NULL
+ */
 int binary_tree_balance(const binary_tree_t *tree)
 {
 	int left_height, right_height;

f92a5456646f83647c25a3357c55a5cf3d81687c
root <ray.dotropg@gmail.com>
2024-12-12 00:26:48 +0000

    correction of 12

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -8,16 +8,12 @@
 
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
-	size_t leaves_left = 0;
-	size_t leaves_right = 0;
-
-
 	if (tree == NULL)
 		return (0);
 
 	if (tree->left == NULL && tree->right == NULL)
 		return (1);
 
-	return (leaves_left + leaves_right);
+	return (binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right));
 }
 
